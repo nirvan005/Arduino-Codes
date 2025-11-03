d93a34df45d2ffffba11b5d2943da1449e5e66a1
Nirvan Jain <nirvanjain2000@gmail.com>
2025-11-04 02:33:24 +0530

    Phone Keypad Interface commit

---
+++ KeypadPhoneArduino/KeypadPhoneArduino.ino
@@ -0,0 +1,29 @@
+#include<Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 3;
+
+char keys[ROWS][COLS]={
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'},
+};
+
+byte rowPins[ROWS]={9,8,7,6};
+byte colPins[COLS]={5,4,3};
+
+Keypad customKeypad=Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Keypad Test Started. Press any key...");
+}
+
+void loop() {
+  char customKey=customKeypad.getKey();
+  if(customKey){
+    Serial.print("Key Pressed: ");
+    Serial.println(customKey);
+  }
+}
